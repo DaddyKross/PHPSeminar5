1c0c46cabe4ff3ac6da40df9db164db24b76afdd
Belova Anastasiya <kitsunekrossasmr@gmail.com>
2024-10-08 00:16:52 +0300

    Seminar 6 added and done

---
+++ code/src/Domain/Controllers/infoContreller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Geekbrains\Application1\Controllers;
+
+use Geekbrains\Application1\Models\SiteInfo;
+use Geekbrains\Application1\Render;
+
+class infoController
+{
+    public function actionIndex(): string
+    {
+        $siteInfo = new SiteInfo();
+        //$info = $siteInfo->getInfo();
+        $render = new Render();
+        return $render->renderPage('site-info.twig', [
+            "server" => $siteInfo->getWebServer(),
+            "phpVersion" => $siteInfo->getPhpVersion(),
+            "userAgent" => $siteInfo->getUserAgent(),
+        ]);
+    }
+
+}
